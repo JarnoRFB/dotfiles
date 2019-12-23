da335ed6061827cd1846fbb171f4256914e55ae7
Rüdiger Busche <rbusche@inserve.de>
2019-12-23 10:03:15 +0100

    WIP

--- config.fish
+++ config.fish
@@ -1,11 +1,13 @@
-conda activate base
-
 set -gx PATH ~/scripts $PATH
 set -g theme_display_date no
 set -g theme_display_cmd_duration no
+set -g theme_powerline_fonts yes
 set -g theme_newline_cursor yes
-set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
 
-function fish_right_prompt
-  echo $CONDA_PROMPT_MODIFIER
-end
+conda activate base
+
+powerline-daemon -q
+set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
+powerline-setup
+
+
