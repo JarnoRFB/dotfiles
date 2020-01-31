759a3e4745589ff4e1d1808211bdac94154a106c
rbusche <rbusche@inserve.de>
2020-01-31 21:23:47 +0100

    Add gnome setting for joint workspaces

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 cp de /usr/share/X11/xkb/symbols
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'de'), ('xkb', 'de+T3')]"
+
+gsettings set org.gnome.mutter workspaces-only-on-primary false
+
 apt install powerline
+
 git config --global push.default current
